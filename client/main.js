3039b16984bdf51af3c465a638f9dc1de426f2db
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-05-29 22:58:28 -0600

    added link list component

--- client/main.js
+++ client/main.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom';
 import {Links} from '../imports/collections/links';
 import Header from './components/header';
 import LinkCreate from './components/link_create';
+import LinkList from './components/link_list';
 
 const App = () => {
 	return (
 		<div>
 			<Header />
 			<LinkCreate />
+			<LinkList />
 		</div>
 	);
 };
